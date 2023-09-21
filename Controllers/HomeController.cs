a0be87c4a8e9e443dd9f62f16799af7a52e853b8
Simon Schwartz <sschwartz794@gmail.com>
2023-09-21 14:58:57 -0500

    Updated Landing Page and Broke email

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using TrackingBugs.Data;
@@ -15,34 +16,38 @@ namespace TrackingBugs.Controllers
     public class HomeController : BTBaseController
     {
         private readonly ILogger<HomeController> _logger;
-		private readonly ApplicationDbContext _context;
-		private readonly UserManager<BTUser> _userManager;
-		private readonly IProjectService _projectService;
-		private readonly IBTRoleService _roleService;
-		private readonly IBTCompanyService _companyService;
-		private readonly IBTTicketService _ticketService;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<BTUser> _userManager;
+        private readonly IProjectService _projectService;
+        private readonly IBTRoleService _roleService;
+        private readonly IBTCompanyService _companyService;
+        private readonly IBTTicketService _ticketService;
+        private readonly IEmailSender _emailSender;
+        private readonly IConfiguration _configuration;
 
-		public HomeController(ILogger<HomeController> logger, UserManager<BTUser> userManager, ApplicationDbContext context, IProjectService projectService, IBTRoleService roleService, IBTTicketService ticketService, IBTCompanyService companyService)
-		{
-			_logger = logger;
-			_userManager = userManager;
-			_context = context;
-			_projectService = projectService;
-			_roleService = roleService;
-			_ticketService = ticketService;
-			_companyService = companyService;
-		}
+        public HomeController(ILogger<HomeController> logger, UserManager<BTUser> userManager, ApplicationDbContext context, IProjectService projectService, IBTRoleService roleService, IBTTicketService ticketService, IBTCompanyService companyService, IConfiguration configuration, IEmailSender emailSender)
+        {
+            _logger = logger;
+            _userManager = userManager;
+            _context = context;
+            _projectService = projectService;
+            _roleService = roleService;
+            _ticketService = ticketService;
+            _companyService = companyService;
+            _configuration = configuration;
+            _emailSender = emailSender;
+        }
 
-		public IActionResult Index()
+        public IActionResult Index()
         {
             return View();
         }
         [Authorize]
-		[HttpGet]
+        [HttpGet]
         public async Task<IActionResult> DashboardAsync(int? id)
-		{
+        {
 
-			DashboardVM viewModel = new()
+            DashboardVM viewModel = new()
             {
                 Company = await _companyService.GetCompanyInfoAsync(_companyId),
                 Projects = await _projectService.GetUnassignedProjectsByCompanyIdAsync(_companyId),
@@ -52,10 +57,10 @@ namespace TrackingBugs.Controllers
 
 
             };
-			return View(viewModel);
-		}
+            return View(viewModel);
+        }
 
-		public IActionResult Privacy()
+        public IActionResult Privacy()
         {
             return View();
         }
@@ -70,77 +75,103 @@ namespace TrackingBugs.Controllers
         {
             return View();
         }
-		[HttpPost]
-		public async Task<JsonResult> PlotlyBarChart()
-		{
-			PlotlyBarData plotlyData = new();
-			List<PlotlyBar> barData = new();
+        [HttpPost]
+        public async Task<JsonResult> PlotlyBarChart()
+        {
+            PlotlyBarData plotlyData = new();
+            List<PlotlyBar> barData = new();
 
-			int companyId = User.Identity.GetCompanyId();
+            int companyId = User.Identity.GetCompanyId();
 
-			List<Project> projects = await _projectService.GetAllProjectsByCompanyIdAsync(_companyId);
+            List<Project> projects = await _projectService.GetAllProjectsByCompanyIdAsync(_companyId);
 
-			//Bar One
-			PlotlyBar barOne = new()
-			{
-				X = projects.Select(p => p.Name).ToArray(),
-				Y = projects.SelectMany(p => p.Tickets).GroupBy(t => t.ProjectId).Select(g => g.Count()).ToArray(),
-				Name = "Tickets",
-				Type = "bar"
-			};
+            //Bar One
+            PlotlyBar barOne = new()
+            {
+                X = projects.Select(p => p.Name).ToArray(),
+                Y = projects.SelectMany(p => p.Tickets).GroupBy(t => t.ProjectId).Select(g => g.Count()).ToArray(),
+                Name = "Tickets",
+                Type = "bar"
+            };
 
-			//Bar Two
-			PlotlyBar barTwo = new()
-			{
-				X = projects.Select(p => p.Name).ToArray(),
-				Y = projects.Select(async p => (await _projectService.GetProjectMembersByRoleAsync(p.Id, nameof(BTRoles.Developer),_companyId)).Count).Select(c => c.Result).ToArray(),
-				Name = "Developers",
-				Type = "bar"
-			};
+            //Bar Two
+            PlotlyBar barTwo = new()
+            {
+                X = projects.Select(p => p.Name).ToArray(),
+                Y = projects.Select(async p => (await _projectService.GetProjectMembersByRoleAsync(p.Id, nameof(BTRoles.Developer), _companyId)).Count).Select(c => c.Result).ToArray(),
+                Name = "Developers",
+                Type = "bar"
+            };
 
-			barData.Add(barOne);
-			barData.Add(barTwo);
+            barData.Add(barOne);
+            barData.Add(barTwo);
 
-			plotlyData.Data = barData;
+            plotlyData.Data = barData;
 
-			return Json(plotlyData);
-		}
+            return Json(plotlyData);
+        }
 
-		[HttpPost]
-		public async Task<JsonResult> GglProjectTickets()
-		{
-			int companyId = User.Identity.GetCompanyId();
+        [HttpPost]
+        public async Task<JsonResult> GglProjectTickets()
+        {
+            int companyId = User.Identity.GetCompanyId();
 
-			List<Project> projects = await _projectService.GetAllProjectsByCompanyIdAsync(_companyId);
+            List<Project> projects = await _projectService.GetAllProjectsByCompanyIdAsync(_companyId);
 
-			List<object> chartData = new();
-			chartData.Add(new object[] { "ProjectName", "TicketCount" });
+            List<object> chartData = new();
+            chartData.Add(new object[] { "ProjectName", "TicketCount" });
 
-			foreach (Project prj in projects)
-			{
-				chartData.Add(new object[] { prj.Name, prj.Tickets.Count() });
-			}
+            foreach (Project prj in projects)
+            {
+                chartData.Add(new object[] { prj.Name, prj.Tickets.Count() });
+            }
 
-			return Json(chartData);
-		}
-		[HttpPost]
-		public async Task<JsonResult> GglProjectPriority()
-		{
-			int companyId = User.Identity.GetCompanyId();
+            return Json(chartData);
+        }
+        [HttpPost]
+        public async Task<JsonResult> GglProjectPriority()
+        {
+            int companyId = User.Identity.GetCompanyId();
 
-			List<Project> projects = await _projectService.GetAllProjectsByCompanyIdAsync(_companyId);
+            List<Project> projects = await _projectService.GetAllProjectsByCompanyIdAsync(_companyId);
 
-			List<object> chartData = new();
-			chartData.Add(new object[] { "Priority", "Count" });
+            List<object> chartData = new();
+            chartData.Add(new object[] { "Priority", "Count" });
 
 
-			foreach (string priority in Enum.GetNames(typeof(BTProjectPriorities)))
-			{
-				int priorityCount = (await _projectService.GetAllProjectsByPriorityAsync(companyId, priority)).Count();
-				chartData.Add(new object[] { priority, priorityCount });
-			}
+            foreach (string priority in Enum.GetNames(typeof(BTProjectPriorities)))
+            {
+                int priorityCount = (await _projectService.GetAllProjectsByPriorityAsync(companyId, priority)).Count();
+                chartData.Add(new object[] { priority, priorityCount });
+            }
 
-			return Json(chartData);
-		}
-	}
+            return Json(chartData);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ContactMe([Bind("FirstName,LastName,Email")] string? message)
+        {
+            string? swalMessage = string.Empty;
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    string? adminEmail = _configuration["AdminLoginEmail"] ?? Environment.GetEnvironmentVariable("AdminLoginEmail");
+                    await _emailSender.SendEmailAsync(adminEmail!, "Contact Me Message From - Tracker", message!);
+                    swalMessage = "Email sent successfully!";
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+                swalMessage = "Error: Unable to send email.";
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
 }
